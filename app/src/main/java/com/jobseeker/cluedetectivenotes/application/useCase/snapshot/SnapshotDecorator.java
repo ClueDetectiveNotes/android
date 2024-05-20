de7cf350c3823fec04a5a6b43bc699fe76b759d1
eunwoo kim <sthalunar@gmail.com>
2024-05-20 23:28:42 +0900

    :recycle: SnapshotManager 싱글톤으로 변경

--- app/src/main/java/com/jobseeker/cluedetectivenotes/application/useCase/snapshot/SnapshotDecorator.java
+++ app/src/main/java/com/jobseeker/cluedetectivenotes/application/useCase/snapshot/SnapshotDecorator.java
@@ -1,17 +1,14 @@
 package com.jobseeker.cluedetectivenotes.application.useCase.snapshot;
 
-import android.util.Log;
-
 import com.jobseeker.cluedetectivenotes.application.useCase.UseCase;
 import com.jobseeker.cluedetectivenotes.application.useCase.UseCaseDecorator;
 import com.jobseeker.cluedetectivenotes.domain.model.snapshot.SnapshotManager;
 
 public class SnapshotDecorator<V> extends UseCaseDecorator<V> {
-    private final SnapshotManager snapshotManager = new SnapshotManager();
+    private final SnapshotManager snapshotManager = SnapshotManager.getInstance();
     public SnapshotDecorator(UseCase<V> useCase){
         super(useCase);
     }
-
     @Override
     public <T> V execute(T param) throws Exception {
         V result = wrappee.execute(param);
--- app/src/main/java/com/jobseeker/cluedetectivenotes/domain/model/snapshot/SnapshotManager.java
+++ app/src/main/java/com/jobseeker/cluedetectivenotes/domain/model/snapshot/SnapshotManager.java
@@ -3,14 +3,19 @@ package com.jobseeker.cluedetectivenotes.domain.model.snapshot;
 import java.util.Stack;
 
 public class SnapshotManager {
+    static SnapshotManager snapshotManager;
     Snapshot tempSnapshot = new Snapshot();
     Stack<Snapshot> snapshotStack = new Stack<>();
-
+    public static SnapshotManager getInstance(){
+        if(snapshotManager == null){
+            snapshotManager = new SnapshotManager();
+        }
+        return snapshotManager;
+    }
     public void takeSnapshot(){
         snapshotStack.push(tempSnapshot);
         tempSnapshot = new Snapshot();
     }
-
     public Snapshot popOffSnapshot() throws Exception{
         if(snapshotStack.isEmpty()) throw new Exception();
         return snapshotStack.pop();
